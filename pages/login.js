4ab247ff7d840bf8f45ea37ed5b0545649e1b268
Juliana Choi <julianachoi@Julianas-MacBook-Pro.local>
2022-09-24 16:22:31 +0900

    trying to merge aaa

--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,33 @@
+import Stack from "react-bootstrap/Stack";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import styles from "../styles/Login.module.scss";
+import ListGroup from "react-bootstrap/ListGroup";
+import { useRouter } from "next/router";
+import Link from "next/link";
+
+
 export default function login() {
-    
+    const router = useRouter();
+
+    const goToMainPage = () => {
+        router.push("/main");
+    }
+    return(
+        <Stack direction="vertical" className={styles.mainBody} gap={2}>
+        <Row className={styles.header} xs="auto">
+            <Col>
+            <h3>Your address</h3>
+            </Col>
+            <Col>
+            <h3>xxxxx</h3>
+            </Col>
+        </Row>
+
+        </Stack>
+
+
+    );
     
 }
